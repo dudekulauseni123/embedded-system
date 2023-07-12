d0088f624dbcb70dcef5c1dbd4d25256021174fb
dudekulauseni123 <r171099@rguktrkv.ac.in>
2023-07-12 11:30:06 +0530

    updated part

---
+++ vaman/vaman-esp/bluetooth/codes/.pio/build/esp32doit-devkit-v1/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,12 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/stdlib_noniso.c.o: \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /home/hussain/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
